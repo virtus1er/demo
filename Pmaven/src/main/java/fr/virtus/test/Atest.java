5d20cb145acceca1b15fb5a9a965a0d1720948fd
virtus1er <36334876+virtus1er@users.noreply.github.com>
2018-02-12 22:45:35 +0100

    test

---
+++ Pmaven/src/main/java/fr/virtus/test/Atest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package fr.virtus.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author sylvain
+ *
+ */
+public class Atest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
